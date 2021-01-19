9a87733a5a9140ba59c002bdc16aa22828b01e0a
fede sirna <fede_sirna@hotmail.com>
2021-01-19 08:39:20 -0300

    commit

---
+++ script/index.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+/*  MENÚ HAMBURGUESA  */
+let btnmenu = document.getElementById('menu-burguer');
+btnmenu.addEventListener("click", function () {
+    document.getElementById('ul-menu').classList.toggle("menudesplegado");
+    
+})
