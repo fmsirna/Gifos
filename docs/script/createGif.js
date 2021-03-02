45ba4f998884bb5d7bdff13a952bd81b549163ef
federico <fede_sirna@hotmail.com>
2021-03-02 11:01:24 -0300

    test

---
+++ docs/script/createGif.js
@@ -0,0 +1,156 @@
+let div_descripcion = document.getElementById('div-descripcion');
+let steps = document.getElementsByClassName('step');
+let counter = document.getElementById('counter-recording');
+let img_gif = document.getElementById('gif-recorded');
+let img_uploadedGif = document.getElementById('img-uploadedGif');
+let text_uploadedGif = document.getElementById('text-uploadedGif');
+let div_uploadedGif = document.getElementById('div-uploadedGif');
+let options_uploadedGif = document.getElementById('options-uploadedGif');
+
+//buttons
+let btn_begin = document.getElementById('btn-begin');
+let btn_record = document.getElementById('btn-record');
+let btn_stop = document.getElementById('btn-stop');
+let btn_upload = document.getElementById('btn-upload');
+let btn_repeat = document.getElementById('btn-repeat');
+let btn_download = document.getElementById('btn-download');
+let btn_link = document.getElementById('btn-link');
+
+var video = document.querySelector('video');
+let interval, minutes, hours, seconds
+let recorder, gifBlob;
+/*_____________________EVENTOS BOTONES_____________________*/
+btn_begin.addEventListener("click", () => {
+    
+    visual();
+    videoAccess()
+});
+btn_record.addEventListener("click", record);
+btn_stop.addEventListener("click", stopRecord);
+btn_repeat.addEventListener("click", repeatRecording);
+btn_upload.addEventListener("click", uploadGif);
+
+/*_____________________FUNCIONES GRABACIÓN_____________________*/
+function visual() {
+    div_descripcion.innerHTML = "<h2>¿Nos das acceso </br> a tu cámara?</h2><p>El acceso a tu camara será válido sólo por el tiempo en el que estés creando el GIFO.</p>";
+    steps[0].classList.add('stepActive');
+}
+
+function videoAccess() {    
+    btn_begin.hidden = true;
+    var constraints = { audio: false, video: true};    
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(function (stream) {
+            video.srcObject = stream;
+            video.play()            
+            recorder = RecordRTC(stream, {
+                type: 'gif',
+                frameRate: 1,
+                quality: 10,
+                width: 360,                
+                onGifRecordingStarted: function() {
+                 console.log('started')
+               },
+              });
+            btn_record.hidden = false;
+
+        }).catch(function (error) {
+            alert('Debes permitir el acceso a tu cámara para continuar');
+            console.error(error);
+            btn_begin.hidden = false;
+        });
+}
+
+
+function record() {
+    recorder.startRecording();
+    btn_record.hidden = true;
+    btn_stop.hidden = false;
+    counter.hidden = false;
+    steps[0].classList.remove('stepActive');
+    steps[1].classList.add('stepActive');
+    
+
+    /*_____________________CALCULATE RECORD TIMING_____________________*/
+    seconds = 0, minutes = 0, hours = 0;
+    interval = setInterval(() => {
+        seconds++;
+        counter.innerHTML = seconds;
+        if (seconds == 60) {
+            minutes++;
+            seconds = 0;
+        }
+        if (minutes == 60) {
+            hours++;
+            minutes = 0;
+        }
+        counter.innerHTML = `${hours}:${minutes}:${seconds}`;
+    }, 1000);
+}
+
+function stopRecord() {
+    recorder.stopRecording(function () {
+        gifBlob = recorder.getBlob()
+    });    
+    img_gif.hidden = false;
+    img_gif.src = URL.createObjectURL(gifBlob);  
+    btn_upload.hidden = false;
+    btn_stop.hidden = true;        
+    counter.hidden = true;
+    btn_repeat.hidden = false;
+    steps[1].classList.remove('stepActive');
+    steps[2].classList.add('stepActive');
+}
+
+function repeatRecording() {
+    recorder.clearRecordedData();
+    img_gif.hidden = true;
+    btn_upload.hidden = true;
+    btn_repeat.hidden = true;  
+    btn_begin.hidden = false;
+    steps[2].classList.remove('stepActive');
+    steps[0].classList.add('stepActive');    
+    videoAccess()       
+}
+
+async function uploadGif() {
+    btn_upload.hidden = true;    
+    btn_repeat.hidden = true;
+    div_uploadedGif.classList.add('div-uploadedGif')    
+    let data = new FormData();
+    data.append('file', gifBlob, 'migif.gif')    
+    const response = await fetch(`https://upload.giphy.com/v1/gifs?api_key=${APIkey}
+    `, {
+      method: 'POST',
+      body: data
+    })
+    const responseJson = await response.json()    
+    AddToMyGifs(responseJson.data.id)    
+    loadGifInfo(responseJson.data.id);    
+    img_uploadedGif.srcset = 'images/check.svg';
+    text_uploadedGif.innerHTML = "GIFO subido con éxito";  
+    options_uploadedGif.classList.add('options-uploadedGif') 
+}
+/*_____________________ADD MY GIFS TO LOCAL STORAGE_____________________*/
+function AddToMyGifs(myGifId) {
+    myGifs_list = getMyGifs()
+    myGifs_list.push(myGifId);
+    localStorage.setItem('myGifs', JSON.stringify(myGifs_list));
+}
+async function loadGifInfo(idGif){    
+    let data = await fetch(`https://api.giphy.com/v1/gifs/${idGif}?api_key=${APIkey}`);
+    let gifInfo = await data.json();
+    //Events for buttons
+    btn_link.addEventListener("click", function copyLink() {
+        var enlace = gifInfo.data.url;
+        var inputTemporalLink = document.createElement("input");
+        inputTemporalLink.setAttribute("value", enlace);
+        document.body.appendChild(inputTemporalLink);
+        inputTemporalLink.select();
+        document.execCommand("copy");
+        document.body.removeChild(inputTemporalLink);
+    });
+    btn_download.addEventListener("click", () => {
+        downloadGif(gifInfo.data.images.original.url,'My Gif');
+    });
+}    
---
+++ docs/script/global.js
@@ -0,0 +1,173 @@
+const APIkey = "4XJBKHwCh7IhhV3ZHOa3c8VaaOarlzMX";
+var tema = 'light';
+
+//Search Elements (input)
+let input_search = document.getElementById('in-busqueda');
+let lista_autocomplete = document.getElementById('lista-autocomplete');
+let lupa_busqueda = document.getElementById('lupa-busqueda');
+let close_search = document.getElementById('cerrar-busqueda');
+let maxWidth = window.matchMedia("(max-width: 750px)")
+let offset= 0;
+//slider trending Elements
+let lista_trending = document.getElementById('lista-trending');
+let btn_vermas = document.getElementById('btn-vermas');
+let heartFav;
+let titulo = document.getElementById('titulo-resultados');
+let resultados = document.getElementById('resultados-gifs');
+//favorite 
+let container_fav = document.getElementById('favorites-gifs');
+let contenedor_emptyFav = document.getElementById('non-favorites');
+let myGifs_list,favorite_list;
+//my gifs
+let containerMyGifs = document.getElementById('myGifs_gifs');
+let containerEmptyMyGifs = document.getElementById('myGifs_empty');
+
+
+modeChange()
+
+
+/*________________GET DATA FROM LOCAL STORAGE_____________________*/
+function getLocalStorageGifs() {
+    favorite_list = JSON.parse(localStorage.getItem('favoriteGifs'));  
+    if(!favorite_list)
+    {favorite_list=[]}
+    return  favorite_list
+}
+function getMyGifs() {
+    myGifs_list = JSON.parse(localStorage.getItem('myGifs'));  
+    if(!myGifs_list)
+    {myGifs_list=[]}
+    return  myGifs_list
+}
+
+/*________________  BURGUER MENU _______________________ */
+
+let btnmenu = document.getElementById('menu-burguer');
+btnmenu.addEventListener("click", function () {
+document.getElementById('ul-menu').classList.toggle("menudesplegado");    
+})
+
+
+/*_____________________DARK MODE_____________________*/
+
+
+document.getElementById('liModoMocturno').addEventListener("click", function () {
+    tema = localStorage.getItem('mode');    
+    tema = tema === 'dark' ? 'light' : 'dark'; 
+    localStorage.setItem('mode',tema)   
+    modeChange()    
+})
+
+function modeChange() {
+    tema = localStorage.getItem('mode');
+    if (!tema){localStorage.setItem('mode', 'light');}
+    if (tema === 'dark'){
+        document.documentElement.setAttribute('data-theme', 'dark')
+        if (lupa_busqueda){
+           lupa_busqueda.src ='images/icon-search-modo-noct.svg'  
+        }                 
+   }
+   else{
+       document.documentElement.setAttribute('data-theme', 'none')
+       if (lupa_busqueda){
+       document.getElementById('lupa-busqueda').setAttribute('src', 'images/icon-search.svg') 
+       }           
+   } 
+   document.getElementById('sp-tema').innerHTML = tema == 'dark' ? 'Diurno' : 'Nocturno';
+}
+
+
+// to check if gif is already in storage and leave it the heart favorite button active
+function isAfavoriteGif(favoriteId) {      
+    favorite_list = getLocalStorageGifs()
+    let checkDuplciated = favorite_list.find(id => id == favoriteId)
+    if(!checkDuplciated)
+    {      
+    return 1 // to use it as a return value to know if its already there
+    }
+}
+
+
+/*_____________________  GIFS BUTTON FUNCTIONS   ____________________*/
+
+
+async function downloadGif(urlGif,title) {    
+  let a = document.createElement('a');
+  // get image as blob
+  let response = await fetch(urlGif);
+  let file = await response.blob();
+  // use download attribute 
+  a.download = title;
+  a.href = window.URL.createObjectURL(file);
+  //store download url in javascript   
+  //click on element to start download
+  a.click();    
+}
+
+// add to favorite button 
+function addFavorite(favoriteId) {    
+    favorite_list = getLocalStorageGifs()
+    let checkDuplciated = favorite_list.find(id => id == favoriteId)
+    if(!checkDuplciated) // if it is not in local storage, add it
+    {
+    favorite_list.push(favoriteId);
+    localStorage.setItem('favoriteGifs', JSON.stringify(favorite_list));   
+    }
+    else{
+    //else, remove it from local storage, the heart is removed before.
+        let favIndex = favorite_list.indexOf(favoriteId);     
+        favorite_list.splice(favIndex, 1);    
+        localStorage.setItem('favoriteGifs', JSON.stringify(favorite_list));
+        }
+}
+
+/*_____________________ maximize button _____________________*/
+async function maximizeGif(idGif) {
+    if (!!idGif) {        
+        let data = await fetch(`https://api.giphy.com/v1/gifs/${idGif}?api_key=${APIkey}`);
+        let gif = await data.json();
+        let contenedor_maximizado = document.getElementById('gif-max');
+        contenedor_maximizado.style.display = 'flex';
+         // to check if gif is already in storage and leave it the heart favorite button active
+         isAfavoriteGif(gif.data.id)==1 ? heartFav = "images/icon-fav-hover.svg" : heartFav = "./images/icon-fav-active.svg";
+                      
+        contenedor_maximizado.innerHTML =
+            `<button id="btnmax-close" class="close-button">
+                    <img src="images/close.svg" alt="icono-busqueda">
+             </button>
+             <img srcset="${gif.data.images.downsized_large.url}"
+                    alt="${gif.data.id}" id="img-maximizado" class="img-maximizado">
+             <article>
+                   <div>
+                       <p>${gif.data.username}</p>
+                       <p class="titulo">${gif.data.title}</p>
+                   </div>
+                   <div class="maximizado-buttons">
+                       <button id="btnmax-favorito"  class="max-button">
+                           <img src=${heartFav} id="img" alt="icono-busqueda">
+                       </button>
+                       <button id="btnmax-download"  class="max-button"">
+                           <img src="images/icon-download.svg" alt="icono-busqueda">
+                       </button>
+                   </div>
+             </article>`;
+        
+        contenedor_maximizado.querySelector('#btnmax-favorito').addEventListener('click', (e) => {                       
+            let heartImg=  contenedor_maximizado.querySelector('#img')            
+            if (heartImg.getAttribute("src") == "./images/icon-fav-active.svg"){
+             heartImg.src = "./images/icon-fav-hover.svg";              
+            }
+            else{heartImg.src = "./images/icon-fav-active.svg";}
+            addFavorite(gif.data.id);
+            if(container_fav){ // if is in favorites section, load it                
+                loadFavorites()                
+            }      
+        });
+        contenedor_maximizado.querySelector('#btnmax-download').addEventListener('click', () => {
+            downloadGif(gif.data.images.original.url);
+        });
+        contenedor_maximizado.querySelector('#btnmax-close').addEventListener('click', () => {
+            contenedor_maximizado.style.display = 'none';
+        });
+    }
+};
